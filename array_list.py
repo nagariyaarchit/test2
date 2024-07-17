d248a43b4c49424f28d252f276c1e56393ba77be
nagariyaarchit <nagariyaarchit@gmail.com>
2024-07-17 11:59:15 -0700

    added the features of a list

--- array_list.py
+++ array_list.py
@@ -21,15 +21,51 @@ class array_list():
             self.currentSize += 1
         return self.list
 
-        
+    def remove(self, obj):
+        for i in range(0,len(self.list)-1):
+            if (self.list[i]) == obj:
+                self.index = i
+                self.new_list = []
+                for i in range(0,len(self.list)):
+                    if self.index == i:
+                        print("pass")
+                    else:
+                        self.new_list.append(self.list[i])
+                self.list.clear()
+                self.list = self.new_list
+        return self.list
+
+    def reverse(self):
+        self.new_list_r = []
+        self.list_length = len(self.list) - 1
+        for i in range(0,len(self.list)):
+            self.new_list_r.append(self.list[self.list_length])
+            self.list_length -= 1
+        self.list.clear()
+        self.list = self.new_list_r
+
+        return self.list
+
+    def pop(self, index):
+        self.new_list_p = []
+        for i in range(0,len(self.list)-1):
+            if index == i:
+                print("do nothing")
+            else:
+                self.new_list_p.append(self.list[i])
+        self.list.clear()
+        self.list = self.new_list_p
+        return self.list
+
 
 ary = array_list()
 ary.append("hello")
 print(ary.append("obj"))
 for i in range(0,50):
     ary.append(i)
-print(ary.append("g"))
-
+print(ary.remove(34))#cannont remove none
+print(ary.pop(3))
+print(ary.reverse())
 
 
 
